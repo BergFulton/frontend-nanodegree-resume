ba5b734315d7cac11fb39f54f7dc9192f8f4cec9
BergFulton <BergFulton@users.noreply.github.com>
2015-12-27 10:44:12 -0500

    Changed title and job description.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,17 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Tracey Berg-Fulton";
+var formattedName = HTMLheaderName.replace("%data%", name);
+console.log(formattedName);
+console.log(HTMLheaderName);
+
+var role = "Creative Techologist";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+console.log(formattedRole);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var skills = ["HTML", "CSS", "provenance research", "dog snuggling"];
+
+$("#main").append(skills);
+
