5c1ea1e79ac6104c121a8dc642b2ab9f01c211cb
BergFulton <BergFulton@users.noreply.github.com>
2015-12-31 17:35:17 -0500

    Cleaned out Resumebuilder.js, added junkdrawer.js to store code that was written too soon.

---
+++ js/junkdrawer.js
@@ -0,0 +1,49 @@
+var educationCity = HTMLschoolLocation.replace("%data%", education["city"]);
+$("#education").append(educationCity);
+
+var formattedPosition = HTMLworkTitle.replace("%data%", work.currentPosition);
+$("#workExperience").append(formattedPosition);
+
+var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer)
+$('#workExperience').append(formattedEmployer);
+
+
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+$('#header').prepend(formattedBioPic);
+
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+$('#header').append(formattedWelcome); 
+
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+$('#header').append(formattedSkills);
+
+var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contact);
+$('#topContacts').append(formattedContact);
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+$('#topContacts').append(formattedMobile);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
+$('#topContacts').append(formattedEmail);
+
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
+$('#topContacts').append(formattedTwitter);
+
+var formattedGitHub = HTMLgithub.replace("%data%", bio.contact.github);
+$('#topContacts').append(formattedGitHub);
+
+var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
+$('#topContacts').append(formattedLocation);
+
+var name = "Tracey Berg-Fulton";
+var formattedName = HTMLheaderName.replace("%data%", name);
+console.log(formattedName);
+console.log(HTMLheaderName);
+
+var role = "Creative Techologist";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+console.log(formattedRole);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,18 +1,46 @@
-var name = "Tracey Berg-Fulton";
-var formattedName = HTMLheaderName.replace("%data%", name);
-console.log(formattedName);
-console.log(HTMLheaderName);
 
-var role = "Creative Techologist";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-console.log(formattedRole);
+var work = {
+	"jobs": [
+	{
+		"employer":"Carnegie Museum of Art",
+		"title":"Provenance Researcher and Collections Database Associate",
+		"location":"Pittsburgh, PA",
+		"dates": "July 2014-present",
+		"description": "Provenance researcher and data lead for the Art Tracks project."
+	},
+	{
+		"employer": "Shoefitr",
+		"title": "Data Manager",
+		"location": "Pittsburgh, PA",
+		"dates": "July 2013-July 2014",
+		"description": "Managed data intake, data quality, and supervised team of 10."
+	},
+	{
+		"employer": "August Wilson Center for African American Culture",
+		"title": "Registrar and Collections Specialist",
+		"location": "Pittsburgh, PA",
+		"dates": "2012-July 2013",
+		"description": "Responsible for all collections activity, loans, exhibitions, and inventory."
+	}
+]
+}
+
+var projects = {
+	"projects": [{
+		"title": "A Title",
+		"dates": 2015,
+		"description": "A description!"
+		"images": []
+
+	}]
+
+}
 
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
 var bio ={
-	"name" : 'Tracey',
+	"name" : 'Tracey Berg-Fulton',
 	"role" :'Lady who does stuff',
+	"welcomeMessage": "Cuppa tea and a chat?",
 	"contact" : {
 		"mobile": '412-443-2654',
 		"email":'traceybergfulton@gmail.com',
@@ -20,59 +48,49 @@ var bio ={
 		"twitter": '@BergFulton',
 		"location": 'Pittsburgh, PA'
 	},
-	
-	"welcomeMessage": "Cuppa tea and a chat?",
 	"skills": [
 	"HTML ", " CSS", " JavaScript", " RWD", " project management", 
 	" cat herding"],
 	"bioPic": "images/headshot.jpg"
 };
 
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-$('#header').prepend(formattedBioPic);
-
-var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-$('#header').append(formattedWelcome); 
-
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-$('#header').append(formattedSkills);
-
-// var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contact);
-// $('#topContacts').append(formattedContact);
-
-var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
-$('#topContacts').append(formattedMobile);
-
-var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
-$('#topContacts').append(formattedEmail);
-
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
-$('#topContacts').append(formattedTwitter);
-
-var formattedGitHub = HTMLgithub.replace("%data%", bio.contact.github);
-$('#topContacts').append(formattedGitHub);
-
-var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
-$('#topContacts').append(formattedLocation);
-
-var work = {
-};
-work.currentPosition = 'Provenance Researcher and Collections Database Associate';
-work.employer = 'Carnegie Museum of Art';
-work.yearsWorked = '2014-present';
-work.city = 'Pittsburgh, PA';
-
-var formattedPosition = HTMLworkTitle.replace("%data%", work.currentPosition);
-$("#workExperience").append(formattedPosition);
-//var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer)
-//$('#workExperience').append(formattedEmployer);
 
 
-var education = {};
-education["name"] = 'University of Glasgow';
-education["degree"] = 'MLitt, with Merit';
-education["years"] = '2007-2008';
-education["city"] = 'Glasgow, Scotland';
 
-var educationCity = HTMLschoolLocation.replace("%data%", education["city"]);
-$("#education").append(educationCity);
+var education = {
+	"schools": [{
+		"name": "University of Glasgow",
+		"city": "Glasgow, Scotland",
+		"degree": "MLitt, with Merit",
+		"major": "Decorative Arts and Design History",
+		"grad year": "2008"
+
+	}, {
+		"name": "Otterbein College",
+		"city": "Westerville, Ohio",
+		"degree": "BA, magna cum laude with honors",
+		"major": ["Art", "Journalism"],
+		"grad year": "2007"
+	}],
+	"onlineCourses": [{
+		"title": "Front End Web Development NanoDegree",
+		"school": "Udacity",
+		"dates": "2015-2016",
+		"url": "http://www.udacity.com"
+	}, {
+		"title": "How to Make a Website",
+		"school": "TeamTreehouse",
+		"dates": "2015",
+		"url": "https://teamtreehouse.com/library/how-to-make-a-website"
+	}, {
+		"title": "CSS Basics",
+		"school": "TeamTreehouse",
+		"dates": "2015",
+		"url": "https://teamtreehouse.com/library/css-basics"
+	}, {
+		"title": "JavaScript Basics",
+		"school": "TeamTreehouse",
+		"dates": "2015",
+		"url": "https://teamtreehouse.com/library/javascript-basics"
+	}]
+}
