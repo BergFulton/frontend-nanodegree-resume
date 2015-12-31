2a34d1a47c16c5dc8cba889b6bb29fc06eb6ce01
BergFulton <BergFulton@users.noreply.github.com>
2015-12-31 08:37:36 -0500

    Add contact information and begin education listings

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,8 +10,57 @@ console.log(formattedRole);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+var bio ={
+	"name" : 'Tracey',
+	"role" :'Lady who does stuff',
+	"contact" : {
+		"mobile": '412-443-2654',
+		"email":'traceybergfulton@gmail.com',
+		"github": 'BergFulton',
+		"twitter": '@BergFulton',
+		"location": 'Pittsburgh, PA'
+	},
+	
+	"welcomeMessage": "Cuppa tea and a chat?",
+	"skills": [
+	"HTML ", " CSS", " JavaScript", " RWD", " project management", 
+	" cat herding"],
+	"bioPic": "images/headshot.jpg"
+};
 
-var skills = ["HTML", "CSS", "provenance research", "dog snuggling"];
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+$('#header').prepend(formattedBioPic);
 
-$("#main").append(skills);
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+$('#header').append(formattedWelcome); 
 
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+$('#header').append(formattedSkills);
+
+// var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contact);
+// $('#topContacts').append(formattedContact);
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+$('#topContacts').append(formattedMobile);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
+$('#topContacts').append(formattedEmail);
+
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
+$('#topContacts').append(formattedTwitter);
+
+var formattedGitHub = HTMLgithub.replace("%data%", bio.contact.github);
+$('#topContacts').append(formattedGitHub);
+
+var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
+$('#topContacts').append(formattedLocation);
+
+var work = {
+};
+work.currentPosition = 'Provenance Researcher and Collections Database Associate';
+work.employer = 'Carnegie Museum of Art';
+work.yearsWorked = '2014-present';
+work.city = 'Pittsburgh, PA';
+
+var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer)
+$('#workExperience').append(formattedEmployer);
